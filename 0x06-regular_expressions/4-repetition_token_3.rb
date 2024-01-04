5b6823166411ca4813a11888c83252df68ed4ac9
Siphephelo Khanyile <siphephelokhanyile@gmail.com>
2024-01-04 04:29:04 +0200

    checker requirement fix

--- 0x06-regular_expressions/4-repetition_token_3.rb
+++ 0x06-regular_expressions/4-repetition_token_3.rb
@@ -1,2 +1,3 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/\b(?!hbon\b)(hbn|hbtn|hbttn|hbtttn|hbttttn)\b/).join
+#puts ARGV[0].scan(/\b(?!hbon\b)(hbn|hbtn|hbttn|hbtttn|hbttttn)\b/).join
+puts ARGV[0].scan(/\b(?!hbon\b)\w*/).join
