d32c578f0eca2b9120c0990d93cb2f9cd448d529
Siphephelo Khanyile <siphephelokhanyile@gmail.com>
2024-01-04 16:45:33 +0200

    task 4 checker fix

--- 0x06-regular_expressions/4-repetition_token_3.rb
+++ 0x06-regular_expressions/4-repetition_token_3.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
 #puts ARGV[0].scan(/\b(?!hbon\b)(hbn|hbtn|hbttn|hbtttn|hbttttn)\b/).join
 #puts ARGV[0].scan(/\b(?!hbon\b)\w*/).join
-puts ARGV[0].scan(/hb(n|t{1,4})n?/).join
+puts ARGV[0].scan(/\b(?!hbon\b)(hb(n|t{1,4}))n{1}?/).join
