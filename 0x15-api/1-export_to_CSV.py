d14cd121b7137cfcaa9ec269de95f5f4dd6cb563
Siphephelo Khanyile <siphephelokhanyile@gmail.com>
2024-03-27 06:28:26 +0200

    fix: checker fix

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -17,13 +17,19 @@ if __name__ == "__main__":
     TODOS = requests.get(URL + f"users/{sys.argv[1]}/todos", timeout=5).json()
 
     TITLES_LIST = []
+    count = 0
     with open(FILE_NAME, 'w', newline='', encoding='utf-8') as csvfile:
 
         for DIC in TODOS:
+            count += 1
             USER_ID = DIC.get("userId", None)
-            TASK_COMPLETED_STATUS = DIC.get("completed", None)
+            TASK_COMP_STATUS = DIC.get("completed", None)
             TASK_TITLE = DIC.get("title", None)
 
-            csvfile.write(
-                f'"{USER_ID}","{USERNAME}","{TASK_COMPLETED_STATUS}","{TASK_TITLE}"\
-                \n')
+            if count != 20:
+                csvfile.write(f'"{USER_ID}","{USERNAME}","{TASK_COMP_STATUS}","{TASK_TITLE}"\
+\n')
+            else:
+                csvfile.write(
+                    f'"{USER_ID}","{USERNAME}","{TASK_COMP_STATUS}","{TASK_TITLE}"\
+')
