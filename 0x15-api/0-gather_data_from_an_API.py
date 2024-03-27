a41aa5f31608ad30750b1d9caeea03d5c711028c
Siphephelo Khanyile <siphephelokhanyile@gmail.com>
2024-03-27 04:55:30 +0200

    fix: checker fix, renamed variables to be pythonic, and followed checker guideline for output

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,31 +3,30 @@
 Uses `jsonplaceholder` API to retrieve employee to-do's
 Using employee ID passed to script
 """
-import sys
 import requests
+import sys
 
 
 if __name__ == "__main__":
     # https://jsonplaceholder.typicode.com/users/1/todos
-    Url = "https://jsonplaceholder.typicode.com/"
+    URL = "https://jsonplaceholder.typicode.com/"
 
-    user = requests.get(Url + f"users/{sys.argv[1]}", timeout=5).json()
-    username = user.get("name", None)
+    USER = requests.get(URL + f"users/{sys.argv[1]}", timeout=5).json()
+    USERNAME = USER.get("name", None)
 
-    todos = requests.get(Url + f"users/{sys.argv[1]}/todos", timeout=5).json()
-    # todos_list = json.dumps(todos, indent=2)
+    TODOS = requests.get(URL + f"users/{sys.argv[1]}/todos", timeout=5).json()
 
-    complete = 0
-    tasks = 0
+    COMPLETED = 0
+    TASKS = 0
     TASK_TITLE = []
 
-    for dic in todos:
-        tasks = tasks + 1
+    for dic in TODOS:
+        TASKS = TASKS + 1
         if dic.get("completed", None) is True:
-            complete = complete + 1
+            COMPLETED = COMPLETED + 1
             TASK_TITLE.append(dic.get("title"))
 
     print(
-        f"Employee {username} is done with tasks({complete}/{tasks}):")
+        f"Employee {USERNAME} is done with tasks({COMPLETED}/{TASKS}):")
     for title in TASK_TITLE:
-        print(f"\t{title}")
+        print(f"\t {title}")
