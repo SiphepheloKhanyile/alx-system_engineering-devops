131afed8ac4ce868d1c57a8d98dfbed6051e883b
Siphephelo Khanyile <siphephelokhanyile@gmail.com>
2024-04-09 22:35:20 +0200

    fix: se 'allow_redirects=False' to not follow redirections

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -17,11 +17,13 @@ def number_of_subscribers(subreddit):
 
     # headers
     headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"  # noqa: E501
+        'Accept': 'application/json',
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
     }
 
     # Make a GET request to the Reddit API
-    response = requests.get(url, headers=headers, timeout=10)
+    response = requests.get(url, headers=headers,
+                            allow_redirects=False, timeout=10)
 
     # Check if the request was successful
     if response.status_code == 200:
